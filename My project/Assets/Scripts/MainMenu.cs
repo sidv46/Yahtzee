91c0923f0c86bb7f55565e526fbbe72b45780c64
Tomas457 <tomasvw.werk@gmail.com>
2023-09-08 10:58:14 +0200

    final touch opties menu
    de puntjes op de i gezet. je kunt nu naar de opties menu vanaf de game en je kunt ook weer terug naar de main menu

--- My project/Assets/Scripts/MainMenu.cs
+++ My project/Assets/Scripts/MainMenu.cs
@@ -8,6 +8,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     public void QuitGame()
     {
         Debug.Log("Quit Application");
